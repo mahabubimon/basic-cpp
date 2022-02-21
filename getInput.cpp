3701eb43dbdfe3b8399aa3c7fb7b2c7eefb23ba1
mahabubimon <mahabubemon24@gmail.com>
2022-02-21 22:45:40 +0600

    first commit

---
+++ getInput.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter a Number: ";
+
+    cin >> num;
+
+    cout << "Your entered number is:  " << num;
+    getch();
+}
