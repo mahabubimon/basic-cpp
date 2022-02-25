702f70479149e014ac007ab82a6d950392423daa
mahabubimon <mahabubemon24@gmail.com>
2022-02-25 23:22:38 +0600

    leap year logic

---
+++ operator/logical/leap-year.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main() 
+{
+    int year;
+
+    cout<<"Enter a year: ";
+
+    cin>>year;
+
+    if((year%4==0 && year%100!=0) || year%400==0) {
+        cout<<year<<" is leap year.";
+    }
+    else{
+        cout<<year<<" is not leap year.";
+    }
+    
+
+    getch();
+}
