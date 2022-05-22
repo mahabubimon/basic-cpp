e0158ed9a6096a75db826bdfc6f9730e2bd11cf7
mahabubimon <mahabubemon24@gmail.com>
2022-05-22 21:43:46 +0600

    const argument

---
+++ function/constArg.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+void strlen(const char *p){
+    cout<<p<<" from Bangladesh."<<endl;
+}
+int main(){
+
+    strlen("Hello world!");
+
+    return 0;
+}
---
+++ function/inlineFunc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+inline int product(int a, int b){
+    // Not recommended to use below lines with inline functions
+    // static int c=0; // This executes only once
+    // c = c + 1; // Next time this function is run, the value of c will be retained
+    return a*b;
+}
+
+int main(){
+    int a, b;
+    cout<<"Enter the value of a and b"<<endl;
+    cin>>a>>b;
+    cout<<"The product of a and b is "<<product(a,b)<<endl;
+    
+    return 0;
+}
